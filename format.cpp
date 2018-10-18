ef2886ad844792a8d4e7caa306b28684c2ae3eaf
did1335 <did1335@gmail.com>
2018-10-18 22:29:30 +0800

    ver 1

--- format.cpp
+++ format.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "function.h"
 
 string int2String(int number) 
@@ -7,13 +6,3 @@ string int2String(int number)
 	ss << number;
 	return ss.str();
 }
-=======
-#include "function.h"
-
-string int2String(int number) 
-{	
-	std::stringstream ss;
-	ss << number;
-	return ss.str();
-}
->>>>>>> 4e110365a0348ca4440e6b4cff616288f1b01ff8
